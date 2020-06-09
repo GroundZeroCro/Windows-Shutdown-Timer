6180d2cd0eb71f89d6028a83cb1dad5c328d62af
Mario Dujić <liberasoftware@gmail.com>
2020-06-09 12:50:12 +0200

    canceling old thread starting new thread

--- view/View.py
+++ view/View.py
@@ -1,3 +1,5 @@
+import threading
+import time
 import tkinter as tk
 
 root = tk.Tk()
@@ -6,13 +8,34 @@ padding_horizontal = 10
 padding_vertical = (10, 0)
 padding_vertical_last = (10, 10)
 
+thread_running = True
+thread = None
+
 
 def start_shutdown():
     print("Starting windows shutdown")
+    global thread_running
+    global thread
+    thread_running = True
+    thread = threading.Timer(0, thread_worker, (10,))
+    thread.start()
 
 
 def cancel_shutdown():
-    print("Windows shutdown canceled")
+    print("Canceling windows shutdown")
+    global thread_running
+    global thread
+    thread_running = False
+    thread.cancel()
+
+
+def thread_worker(countdown_seconds):
+    for i in range(countdown_seconds):
+        if not thread_running:
+            return
+        print(i)
+        time.sleep(1)
+    print("Shutting down OS")
 
 
 tk.Label(root, text="Hours", font=font).grid(row=0, padx=padding_horizontal, pady=padding_vertical, sticky=tk.W)
